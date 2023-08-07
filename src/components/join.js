bafcc6125ec2fa51c071af870a41b34179081f7a
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2023-08-08 02:53:22 +0530

    comming soon

--- src/components/join.js
+++ src/components/join.js
@@ -7,10 +7,22 @@ import { fadeIn, textVariant, slideIn } from "../utils/motion";
 
 const Join = () => {
     return(
-     <>        <motion.div variants={textVariant()}>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-        <p className={styles.sectionSubText}>Introduction</p>
+     <><section className=" h-screen"><motion.div variants={textVariant()}>
+      
+        <div className=" text-white z-90 h-full flex flex-col justify-between items-center pt-10 relative title">
+          <div className="opacity-100 transform translate-x-0 translate-y-0 translate-z-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0 skew-x-0 flex flex-col items-center skew-y-0 transform-style-preserve-3d inline-block welcome"><span> Welcome</span><span>To AntSwap</span> <span className="descri">Developing a decentralized ecosystem and community Based on Linea Chain.</span></div>
+         
+        </div>
+        <div className="pt-20">
+        <div className="marquee flex  rounded-20 ">
+          <div className="z-1000 bg-yellow-500 text-black text-center flex items-center p-2">News</div>
+  <div className="marquee__inner items-center">
+    <p className="marquee__line w-max">Website Under Maintenance</p>
+  </div>
+</div>
+        </div>
       </motion.div>
+      </section>
     </>
     )
 }

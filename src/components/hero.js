f50ccee8279d66e09ef13827cd5adcbc86e16eee
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2023-09-07 22:47:36 +0530

    nnn

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,10 +4,11 @@ import { textVariant } from "../utils/motion";
 // import video from "../assets/video.wmv"
 import { Link } from "react-router-dom";
 import { logo } from "../assets";
+import bg from "../assets/bgimg.jpg"
 
 const Hero = () => {
   return (
-    <><section className=" h-screen py-20 flex justify-center items-center">
+    <><section className=" h-screen py-20 flex justify-center items-center" style={{ backgroundImage: `url(${bg})`}}>
 
       <div className=" text-white w-[70%] z-90 h-full min-w-[70%] flex items-center title max-sm:w-[100%] max-sl:p-4 max-sl:flex-col max-sl:justify-evenly">
         <div className=" pr-10 opacity-100 transform translate-x-0 w-[60%]  translate-y-0 translate-z-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0 skew-x-0 flex-col items-start flex skew-y-0 transform-style-preserve-3d welcome"><span className=" text-left leading-tight pb-5"> Empowering the Future with Linea</span> <span className="descri text-left leading-normal">AntSwap is a decentralized platform that facilitates the seamless exchange of digital assets and NFTs.

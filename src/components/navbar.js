bf162c755aea57cea893895c68998ca1bf704bbf
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2023-09-17 18:51:54 +0530

    k

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,15 +3,16 @@ import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo,menu, close } from "../assets";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
 
   return (
+    <>
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 absolute top-0 z-20`}
+      className={`${styles.paddingX} w-full flex items-center py-5 absolute top-0 z-20 max-sm:px-3`}
     >
       <div className=" min-w-[80%] flex justify-between items-center max-w-7xl mx-auto max-sl:w-full">
         <Link
@@ -41,53 +42,49 @@ const Navbar = () => {
               className="py-2 cursor-pointer"
               onClick={() => window.open(Link.icon_link, "_blank")}
             >
-              <img src={Link.icon} className="w-6 h-6"/>
+              <img src={Link.icon} className="w-6 h-6" />
             </li>
           ))}
           <div className="btn_wrapper-center">
-              <Link
-                to="https://t.me/Snailbrook_Entry"
-                rel="noopener noreferrer"
-                className="btn is-green w-inline-block"
-              >
-                <div className="btn_text is-green is-small" style={{padding:'8px'}} >Launch App</div>
-              </Link>
-            </div>
+            <Link
+              to="https://t.me/Snailbrook_Entry"
+              rel="noopener noreferrer"
+              className="btn is-green w-inline-block"
+            >
+              <div className="btn_text is-green is-small" style={{ padding: '8px' }} >Launch App</div>
+            </Link>
+          </div>
         </ul>
 
         {/* for mobile navigation */}
-        {/* <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img
-            src={toggle ? close : menu}
-            alt="menu"
-            className="w-[28px] h-[28px] object-contain cursor-pointer"
-            onClick={() => setToggle(!toggle)}
-          />
-          <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absoulte top-30 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-          >
-            <ul className="list-none flex justify-end items-start flex-col gap-4 ">
-              {navLinks.map((Link) => (
-                <li
-                  key={Link.id}
-                  className={`${
-                    active === Link.title ? "text-white" : "text-secondary"
-                  } font-poppins font-medium text-[16px] cursor-pointer `}
-                  onClick={() => {
-                    setToggle(!toggle);
-                    setActive(Link.title);
-                  }}
-                >
-                  <a href={`#${Link.id}`}>{Link.title}</a>
-                </li>
-              ))}
-            </ul>
+          <div className="sm:hidden flex-1 contents tems-center bg-black justify-end ">
+            <img
+              src={toggle ? close : menu}
+              alt="menu"
+              className="w-[28px] h-[28px] object-contain cursor-pointer justify-end"
+              onClick={() => setToggle(!toggle)}
+            />
+            </div>
+            </div>
+            </nav>
+            <div
+              className={`${!toggle ? "hidden" : "flex"
+                } p-6 black-gradient absoulte z-10 bg-black sticky h-[100vh] w-[100vw] justify-center flex-col items-center`}
+            >
+              <ul className="list-none flex items-start flex-col gap-4 ">
+                {navLinks.map((Link) => (
+                  <li
+                    key={Link.id}
+                    className="py-2 cursor-pointer"
+                    onClick={() => window.open(Link.icon_link, "_blank")}
+                  >
+                    <img src={Link.icon} className="w-6 h-6" />
+                  </li>
+                ))}
+              </ul>
           </div>
-        </div> */}
-      </div>
-    </nav>
+          </>
+    
   );
 };
 

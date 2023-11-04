5f6451e8f251334aa2c461a101f618a5c292c56c
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2023-11-04 21:53:41 +0530

    all

--- src/components/OurPartners/OurPartners.js
+++ src/components/OurPartners/OurPartners.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import styles from './ourPartners.module.css';
 import DAO from './DAOStarter-Logo-Green.png'
+import Psalma from "./2-PhotoRoom.png"
+import { Link } from 'react-router-dom';
 const OurPartners = () => {
   return (
     <section className=' pt-44 pb-28 bg-[#13191f] max-sm:pt-28'>  
          <div className={`${styles.container}`}>
       <div className={`${styles.pTHeader} ${styles.TTypeCenter} ${styles.TPadDouble}`}>
-        <h2 className={styles.TTitle}>Our Partners</h2>
+        <h2 className={styles.TTitle}>Our Investors</h2>
       </div>
 
       <div className={`${styles.TPadDouble} ${styles.TContent}`}>
         <div className={styles.TTContent}>
           <div className={styles.TTContainer}>
             <div className={`${styles.company} ${styles.logo} ${styles.gradientBorder}` } id='box'>
-              <img
+              <Link to="https://www.daostarter.pro/" target="_blank"><img
                 src={DAO}
                 alt="Company Logo 1"
                 className={styles.img}
               />
+              </Link>
+              <Link to="https://www.psalmscapital.com/" target="_blank">
+              <img
+                src={Psalma}
+                alt="Company Logo 1"
+                className={styles.img}
+              />
+              </Link>
             </div>
           </div>
         </div>
